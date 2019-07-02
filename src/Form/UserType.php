68b82a8fc9a7f4b503253bc7a7f056dafd6c73cd
Louison <louison.druelle@epitech.eu>
2019-07-02 15:46:32 +0200

    change placeholder

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -6,15 +6,30 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class UserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('username')
-            ->add('email')
-            ->add('password')
+            ->add('username', TextType::class, [
+                'attr' => [
+                    'placeholder' => "Full name"
+                ]
+            ])
+            ->add('email', EmailType::class, [
+                'attr' => [
+                    'placeholder' => "Email"
+                ]
+            ])
+            ->add('password', PasswordType::class, [
+                'attr' => [
+                    'placeholder' => "Password"
+                ]
+            ])
         ;
     }
 
