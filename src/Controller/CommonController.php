a563c658b1cb3eecd52213b267d5453c624844d3
Mérill Téterel <merill.teterel@epitech.eu>
2019-07-03 11:18:55 +0200

    Layout improvement + homepage timeline

--- src/Controller/CommonController.php
+++ src/Controller/CommonController.php
@@ -7,8 +7,20 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class CommonController extends AbstractController
 {
+    public function miniProfile()
+    {
+        return $this->render('common/mini_profile.html.twig', [
+            'post_count' => 69,
+            'followers_count' => 1337
+        ]);
+    }
     public function trends()
     {
         return $this->render('common/trends.html.twig');
+    }
+
+    public function footer()
+    {
+        return $this->render('common/notafooter.html.twig');
     }
 }
