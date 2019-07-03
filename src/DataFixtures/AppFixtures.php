30e93964d94b8da838fe444d077b00b0ed2774f2
Mérill Téterel <merill.teterel@epitech.eu>
2019-07-03 15:18:07 +0200

    Changed join table for chat_participant

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -12,24 +12,31 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        $user = new User();
-        $user->setUsername('Poutouyemoun');
-        $user->setPassword('test');
-        $user->setDisplayName('Gros Chef Bandzi');
-        $user->setEmail("dev@localhost");
-        $manager->persist($user);
+        $users = [
+            ['u' => 'LouisonD', 'n' => 'Xie Xie'],
+            ['u' => 'EmilieL', 'n' => 'Poutouyemoun'],
+            ['u' => 'MerillT', 'n' => 'Le Canard Jaune'],
+            ['u' => 'LouisM', 'n' => 'Croix V Bâton'],
+        ];
 
-        for($i = 1; $i != 5; $i++) {
-            $quack = new Post();
-            $quack->setSender($user);
-            $quack->setContent(str_repeat("quack ", $i));
-            $quack->setSubmitTime(new DateTime());
-            $quack->setMediaUrl('http://french.peopledaily.com.cn/mediafile/201308/16/F201308160908385111850043.jpg');
-            $manager->persist($quack);
+        foreach($users as $u)
+        {
+            $user = new User();
+            $user->setUsername($u['u']);
+            $user->setPassword('test');
+            $user->setDisplayName($u['n']);
+            $user->setEmail("dev@localhost");
+            $manager->persist($user);
+
+            for($i = 1; $i != 2; ++$i) {
+                $quack = new Post();
+                $quack->setSender($user);
+                $quack->setContent(str_repeat("quack ", $i));
+                $quack->setSubmitTime(new DateTime());
+                $quack->setMediaUrl('https://ichef.bbci.co.uk/news/660/media/images/76352000/jpg/_76352619_76346795.jpg');
+                $manager->persist($quack);
+            }
         }
-
-        // $product = new Product();
-        // $manager->persist($product);
 
         $manager->flush();
     }
