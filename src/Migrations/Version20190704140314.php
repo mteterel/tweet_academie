6021d81a4f5b9de1c82e1fd35236e8072c7f5c7e
Mérill Téterel <merill.teterel@epitech.eu>
2019-07-04 16:42:33 +0200

    Profile layout upgrade.

---
+++ src/Migrations/Version20190704140314.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20190704140314 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE upload CHANGE uploader_id uploader_id INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE follower CHANGE unfollow_date unfollow_date DATETIME DEFAULT NULL');
+        $this->addSql('ALTER TABLE chat_conversation CHANGE name name VARCHAR(255) DEFAULT NULL');
+        $this->addSql('ALTER TABLE post CHANGE parent_post_id parent_post_id INT DEFAULT NULL, CHANGE source_post_id source_post_id INT DEFAULT NULL, CHANGE media_url media_url VARCHAR(255) DEFAULT NULL, CHANGE media_type media_type SMALLINT DEFAULT NULL');
+        $this->addSql('ALTER TABLE user CHANGE birth_date birth_date DATE DEFAULT NULL, CHANGE city city VARCHAR(255) DEFAULT NULL');
+        $this->addSql('ALTER TABLE notification CHANGE notification_data notification_data JSON NOT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE chat_conversation CHANGE name name VARCHAR(255) DEFAULT \'NULL\' COLLATE utf8mb4_unicode_ci');
+        $this->addSql('ALTER TABLE follower CHANGE unfollow_date unfollow_date DATETIME DEFAULT \'NULL\'');
+        $this->addSql('ALTER TABLE notification CHANGE notification_data notification_data LONGTEXT NOT NULL COLLATE utf8mb4_bin');
+        $this->addSql('ALTER TABLE post CHANGE parent_post_id parent_post_id INT DEFAULT NULL, CHANGE source_post_id source_post_id INT DEFAULT NULL, CHANGE media_url media_url VARCHAR(255) DEFAULT \'NULL\' COLLATE utf8mb4_unicode_ci, CHANGE media_type media_type SMALLINT DEFAULT NULL');
+        $this->addSql('ALTER TABLE upload CHANGE uploader_id uploader_id INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE user CHANGE birth_date birth_date DATE DEFAULT \'NULL\', CHANGE city city VARCHAR(255) DEFAULT \'NULL\' COLLATE utf8mb4_unicode_ci');
+    }
+}
