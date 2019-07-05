33d41339bffde65b8448cb0f00671f4ffff19de2
Mérill Téterel <merill.teterel@epitech.eu>
2019-07-05 11:20:49 +0200

    Likes are working

--- public/assets/js/post-card.js
+++ public/assets/js/post-card.js
@@ -1,18 +1,24 @@
 $(document).on('click', '.post-card__like-btn', function() {
-    $postCard = $(this).parents('.post-card');
-    postId = $(card).attr('data-post-id');
+    var self = $(this);
+    $postCard = self.parents('.post-card');
+    postId = $($postCard).attr('data-post-id');
 
     $.ajax('/post/' + postId + '/like', {
         dataType: 'json',
         success: function(data) {
-
+            if (data.favorite === true)
+                $(self).removeClass('btn-secondary')
+                    .addClass('btn-danger');
+            else
+                $(self).removeClass('btn-danger')
+                    .addClass('btn-secondary');
         }
     });
 });
 
 $(document).on('click', '.post-card__repost-btn', function() {
     $postCard = $(this).parents('.post-card');
-    postId = $(card).attr('data-post-id');
+    postId = $($postCard).attr('data-post-id');
 
     $.ajax('/post/' + postId + '/repost', {
         dataType: 'json',
@@ -24,7 +30,7 @@ $(document).on('click', '.post-card__repost-btn', function() {
 
 $(document).on('click', '.post-card__delete-btn', function() {
     $postCard = $(this).parents('.post-card');
-    postId = $(card).attr('data-post-id');
+    postId = $($postCard).attr('data-post-id');
 
     $.ajax('/post/' + postId + '/delete', {
         dataType: 'json',
--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -2,16 +2,54 @@
 
 namespace App\Controller;
 
+use App\Entity\Favorite;
 use App\Entity\Post;
 use App\Repository\FavoriteRepository;
+use App\Repository\PostRepository;
+use Doctrine\Common\Persistence\ObjectManager;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PostController extends AbstractController
 {
-    public function like() { }
+    /**
+     * @Route("/post/{id}/like", name="post_like_ajax")
+     */
+    public function like(Post $post, FavoriteRepository $favoriteRepository, ObjectManager $objectManager)
+    {
+        $favorite = $favoriteRepository->findBy([
+            'user' => $this->getUser(),
+            'post' => $post
+        ]);
+
+        /** @var \App\Entity\User $user */
+        $user = $this->getUser();
+
+        if (empty($favorite))
+        {
+            $entry = new Favorite();
+            $entry->setPost($post);
+            $entry->setUser($user);
+            $user->addFavorite($entry);
+
+            $objectManager->persist($entry);
+            $objectManager->flush();
+            return new JsonResponse(['favorite' => true]);
+        }
+        else
+        {
+            foreach($favorite as $f)
+                $objectManager->remove($f);
+
+            $objectManager->flush();
+            return new JsonResponse(['favorite' => false]);
+        }
+
+    }
+
     public function repost() { }
     public function delete() { }
 }
