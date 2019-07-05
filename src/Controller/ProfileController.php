10f02ea9f1895ef9f7e0b282a62e596d4b05f5f3
Louis Michalak <louis.michalak@epitech.eu>
2019-07-05 15:51:30 +0200

    profile controler update for upload

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -18,62 +18,144 @@ class ProfileController extends AbstractController
     /**
      * @Route("/{username}", name="profile_view")
      */
-    public function view(string $username, UserRepository $repository)
+    public function view(string $username, UserRepository $repository,
+                         UploadRepository $uploadRepository)
     {
         $user = $repository->findOneBy(['username' => $username]);
-
         if ($user === null)
             throw $this->createNotFoundException('The user does not exist');
-
+        $upload = new Upload();
+        $formAvatar = $this->createForm(AvatarType::class, $upload);
+        $formBanner = $this->createForm(BannerType::class, $upload);
+        $arrayUploads = $uploadRepository->getImages($user);
+        dump($arrayUploads);
         return $this->render('profile/index.html.twig', [
-            'user' => $user
+            'user' => $user,
+            'formAvatar' => $formAvatar->createView(),
+            'formBanner' => $formBanner->createView(),
+            'images' => $arrayUploads
+        ]);
+    }
+
+    /**
+     * @Route("/uploadAvatar", name="uploadAvatar")
+     */
+    public function uploadAvatar(Request $request, ObjectManager $em)
+    {
+        $user = $this->getUser();
+        $upload = new Upload();
+        $upload->setUploader($user);
+        $formAvatar = $this->createForm(AvatarType::class, $upload);
+        $formAvatar->handleRequest($request);
+        if ($formAvatar->isSubmitted() && $formAvatar->isValid()) {
+            foreach ($user->getUploads() as $key => $item)
+                if ($item->getType() === "avatar") {
+                    $upload = $user->getUploads()[$key];
+                    $lastPic = $this->getParameter('Avatar_directory') .
+                        '/' . $upload->getPath();
+                    unlink($lastPic);
+                }
+            $file = $formAvatar['path']->getData();
+            $filename = md5(random_bytes(20));
+            $file->move($this->getParameter('Avatar_directory'),
+                $filename);
+            $upload->setPath($filename);
+            $upload->setType('avatar');
+            $user->addUpload($upload);
+            $em->persist($upload);
+            $em->flush();
+        }
+        return $this->redirectToRoute('profile_view', [
+            "username" => $user->getUsername()
+        ]);
+    }
+
+    /**
+     * @Route("/uploadBanner", name="uploadBanner")
+     */
+    public function uploadBanner(Request $request, ObjectManager $em)
+    {
+        $user = $this->getUser();
+        $upload = new Upload();
+        $upload->setUploader($user);
+        $formBanner = $this->createForm(BannerType::class, $upload);
+        $formBanner->handleRequest($request);
+        if ($formBanner->isSubmitted() && $formBanner->isValid())
+        {
+            foreach ($user->getUploads() as $key => $item)
+                if ($item->getType() === "banner")
+                {
+                    $upload = $user->getUploads()[$key];
+                    $lastPic = $this->getParameter('Banner_directory').
+                        '/'.$upload->getPath();
+                    unlink($lastPic);
+                }
+            $file = $formBanner['path']->getData();
+            $filename = md5(random_bytes(20));
+            $file->move($this->getParameter('Banner_directory'),
+                $filename);
+            $upload->setPath($filename);
+            $upload->setType('banner');
+            $user->addUpload($upload);
+            $em->persist($upload);
+            $em->flush();
+        }
+        return $this->redirectToRoute('profile_view', [
+            "username" => $user->getUsername()
         ]);
     }
 
     /**
      * @Route("/{username}/following", name="profile_following")
      */
-    public function following(string $username, UserRepository $userRepository, FollowerRepository $repository)
+    public function following(string $username, UserRepository $userRepository,
+                              FollowerRepository $repository,
+                              UploadRepository $uploadRepository)
     {
         $user = $userRepository->findOneBy(['username' => $username]);
-
         if ($user === null)
             throw $this->createNotFoundException('The user does not exist');
-
+        $arrayUploads = $uploadRepository->getImages($this->getUser());
         return $this->render('profile/following.html.twig', [
-            'user' => $user
+            'user' => $user,
+            'images' => $arrayUploads
         ]);
     }
 
     /**
      * @Route("/{username}/followers", name="profile_followers")
      */
-    public function followers(string $username, UserRepository $userRepository, FollowerRepository $repository)
+    public function followers(string $username, UserRepository $userRepository,
+                              FollowerRepository $repository,
+                              UploadRepository $uploadRepository)
     {
         $user = $userRepository->findOneBy(['username' => $username]);
-
         if ($user === null)
             throw $this->createNotFoundException('The user does not exist');
-
+        $arrayUploads = $uploadRepository->getImages($this->getUser());
         return $this->render('profile/followers.html.twig', [
-            'user' => $user
+            'user' => $user,
+            'images' => $arrayUploads
         ]);
     }
 
     /**
      * @Route("/{username}/likes", name="profile_favorites")
      */
-    public function favorites(string $username, UserRepository $userRepository, FavoriteRepository $repository)
+    public function favorites(string $username, UserRepository $userRepository,
+                              FavoriteRepository $repository,
+                              UploadRepository $uploadRepository)
     {
         $user = $userRepository->findOneBy(['username' => $username]);
-
         if ($user === null)
             throw $this->createNotFoundException('The user does not exist');
-
-        return $this->render('profile/favorites.html.twig', [
-            'user' => $user
+        $arrayUploads = $uploadRepository->getImages($this->getUser());
+        return $this->render('profile/followers.html.twig', [
+            'user' => $user,
+            'images' => $arrayUploads
         ]);
     }
+
     /**
      * @Route("/{username}/edit", name="edit_profile")
      */
