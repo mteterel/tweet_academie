1f004c39480dd71a26c7e7f3e0bfae2e022396d4
Mérill Téterel <merill.teterel@epitech.eu>
2019-07-06 17:00:35 +0200

    Display Likes & Reposts on post card

--- src/Controller/CommonController.php
+++ src/Controller/CommonController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Post;
+use App\Repository\FavoriteRepository;
+use App\Repository\PostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -22,5 +25,22 @@ class CommonController extends AbstractController
     public function footer()
     {
         return $this->render('common/notafooter.html.twig');
+    }
+
+    public function postCard(Post $post, PostRepository $postRepository, FavoriteRepository $favoriteRepository)
+    {
+        $repost_count = $postRepository->count([
+            'source_post' => $post
+        ]);
+
+        $favorite_count = $favoriteRepository->count([
+            'post' => $post
+        ]);
+
+        return $this->render('common/post_card_impl.html.twig', [
+            'post' => $post,
+            'repost_count' => $repost_count,
+            'favorite_count' => $favorite_count
+        ]);
     }
 }
