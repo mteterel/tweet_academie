1f4c2707af86aeac98ea924b4fa9b6866ddbca24
Mérill Téterel <merill.teterel@epitech.eu>
2019-07-08 11:23:16 +0200

    Notification system update

--- src/Controller/NotificationsController.php
+++ src/Controller/NotificationsController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Notification;
 use App\Repository\NotificationRepository;
+use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,12 +13,20 @@ class NotificationsController extends AbstractController
     /**
      * @Route("/notifications", name="notifications")
      */
-    public function index(NotificationRepository $repository)
+    public function index(NotificationRepository $repository, ObjectManager $manager)
     {
         $notifications = $repository->findBy([
             'user' => $this->getUser(),
             'is_read' => false
         ]);
+
+        foreach($notifications as $n)
+        {
+            $n->setIsRead(true);
+            $manager->persist($n);
+        }
+
+        $manager->flush();
 
         return $this->render('notifications/index.html.twig', [
             'notifications' => $notifications
@@ -27,13 +36,21 @@ class NotificationsController extends AbstractController
     /**
      * @Route("/mentions", name="mentions")
      */
-    public function mentions(NotificationRepository $repository)
+    public function mentions(NotificationRepository $repository, ObjectManager $manager)
     {
         $notifications = $repository->findBy([
             'user' => $this->getUser(),
             'notification_type' => Notification::TYPE_MENTION,
             'is_read' => false
         ]);
+
+        foreach($notifications as $n)
+        {
+            $n->setIsRead(true);
+            $manager->persist($n);
+        }
+
+        $manager->flush();
 
         return $this->render('notifications/mentions.html.twig', [
             'notifications' => $notifications
