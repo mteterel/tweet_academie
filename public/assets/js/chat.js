4fd41e8b4c440f4b69e95b50b0c947d6e0dada99
Louison <louison.druelle@epitech.eu>
2019-07-10 12:12:43 +0200

    edit chat ajax

--- public/assets/js/chat.js
+++ public/assets/js/chat.js
@@ -1,14 +1,27 @@
 $('#send').click(function( event ) {
     event.preventDefault();
+    var url = window.location.href;
     $.ajax({
         type: 'POST',
-        url: '/messages',
+        url: url,
         data: new FormData(document.getElementById("formMessages")),
         dataType: 'json',
         processData: false,
         contentType: false,
         success: function (data) {
-            alert('ohoh');
+
+            var msg  = $("<div class='message'></div>")
+                .append(
+                    $("<span></span>")
+                        .addClass("date")
+                        .text(data.time + ": "),
+                    $("<span></span>")
+                        .addClass("content")
+                        .text(data.message)
+                )
+                .appendTo(".chat_conversation");
+
+            $('#chat_message_content').val('');
         }
     });
 });
