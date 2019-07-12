1db11a6a960a71fdd7c606dc1db71fcafcfef1eb
elepine <emilie.lepine@epitech.eu>
2019-07-12 15:19:49 +0200

    ajout search content dans les post

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\PostRepository;
 use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
@@ -21,11 +22,20 @@ class SearchController extends AbstractController
     /**
      * @Route("/search_results", name="search_home")
      */
-    public function search_from_home(UserRepository $userRepository){
+    public function search_from_home(UserRepository $userRepository, PostRepository $postRepository){
+        $type = $_GET['search_type'] ?? "post";
+        if ($type ==="people"){
+            $result = $userRepository->search_user($_GET['search_term']);
+        }
+        elseif ($type === "post"){
+            $result = $postRepository->search_post($_GET['search_term']);
+        }
 
-        $result = $userRepository->search_user($_GET['search-info']);
+
         return $this->render('search/index.html.twig', [
-            "results"=>$result
+            "results"=>$result,
+            "search_info"=>$_GET['search_term'],
+            "search_type"=>$type
         ]);
     }
 }
